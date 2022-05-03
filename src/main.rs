44054e37679087a510e66b082800268d5220d7fb
aetn23 <mikolajpirog@gmail.com>
2022-05-03 20:44:23 +0200

    Added petgraph example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use petgraph::Graph;
+
 fn main() {
-    println!("Hello, world!");
+    let mut graph = Graph::<i32,i32>::new();
+    let index = graph.add_node(23);
+    let index2 = graph.add_node(23);
+    graph.add_edge(index, index, 0);
+    graph.add_edge(index, index2, 0);
 }
