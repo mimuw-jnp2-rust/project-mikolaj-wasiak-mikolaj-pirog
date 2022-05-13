cbce8e293867776f212b2736a3d61c5051887f17
Mikołaj Wasiak <m.wasiak11@student.uw.edu.pl>
2022-05-13 22:08:58 +0200

    Arrows

--- src/graph/edge.rs
+++ src/graph/edge.rs
@@ -1,11 +1,15 @@
 use egui_tetra::State;
 use std::error::Error;
+use std::f32::consts::PI;
+use tetra::graphics::mesh::GeometryBuilder;
 use tetra::graphics::{mesh::Mesh, Color, DrawParams};
 use tetra::Context;
 
 use super::Position;
 
-const BASE_STROKE_WIDTH: f32 = 4.0;
+const BASE_STROKE_WIDTH: f32 = 5.;
+const BASE_ARROW_SCALE: f32 = 0.7;
+const BASE_ARROW_ARMS_SIZE: f32 = 25.;
 
 pub struct Edge {
     from: Position,
@@ -16,12 +20,34 @@ pub struct Edge {
 }
 
 impl Edge {
+    fn create_arrow(
+        ctx: &mut Context,
+        from: Position,
+        to: Position,
+    ) -> Result<Mesh, tetra::TetraError> {
+        let (from, to) = (
+            Position::lerp(from, to, (1. - BASE_ARROW_SCALE) / 2.),
+            Position::lerp(from, to, (1. + BASE_ARROW_SCALE) / 2.),
+        );
+        let left_arrow_point =
+            (to - from).rotated_z(PI * 3. / 4.).normalized() * BASE_ARROW_ARMS_SIZE + to;
+        let right_arrow_point =
+            (to - from).rotated_z(-PI * 3. / 4.).normalized() * BASE_ARROW_ARMS_SIZE + to;
+        let mut builder = GeometryBuilder::new();
+        builder.polyline(BASE_STROKE_WIDTH, &[from, to])?;
+        builder.polyline(
+            BASE_STROKE_WIDTH,
+            &[left_arrow_point, to, right_arrow_point],
+        )?;
+        builder.build_mesh(ctx)
+    }
+
     pub fn new(ctx: &mut Context, from: Position, to: Position) -> Result<Edge, Box<dyn Error>> {
         Ok(Edge {
             from,
             to,
             color: Color::BLACK,
-            shape: Mesh::polyline(ctx, BASE_STROKE_WIDTH, &[from, to])?,
+            shape: Edge::create_arrow(ctx, from, to)?,
         })
     }
 
@@ -33,7 +59,7 @@ impl Edge {
     ) -> Result<(), Box<dyn Error>> {
         self.from = from;
         self.to = to;
-        self.shape = Mesh::polyline(ctx, BASE_STROKE_WIDTH, &[from, to])?;
+        self.shape = Edge::create_arrow(ctx, from, to)?;
         Ok(())
     }
 
